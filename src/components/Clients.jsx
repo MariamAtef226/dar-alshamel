2b7b284753cd510b9ebc4cf535562ffc046aec4f
MariamAtef226 <mariamatef226@gmail.com>
2024-02-05 13:15:35 +0200

    initial commit

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -8,6 +8,7 @@ export default function Clients() {
       <h2 className="text-center white mb-0">من نخدم</h2>
 
       <div className="client-types d-flex flex-column flex-md-row pt-5 mt-3 mt-md-5 justify-content-evenly pb-4">
+        
         <div className="one-client d-flex flex-column align-items-center w-100 pb-5 transparent floatUp">
           <div className="container-client bluebg p-5 text-center pt-auto pb-0 ms-auto me-auto">
             <img src={factory} className="ms-auto me-auto" />
